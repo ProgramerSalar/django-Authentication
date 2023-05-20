1cea2a6505abb61d64801fddf119006fb7dcf93e
ProgramerSalar <manishkumar60708090@gmail.com>
2023-05-20 17:10:23 +0530

    register on the database

--- accounts/forms.py
+++ accounts/forms.py
@@ -18,6 +18,21 @@ class RegistrationForm(forms.ModelForm):
         fields = ['first_name', 'last_name', 'phone_number', 'email', 'password']
         
         
+        
+     
+    def clean(self):
+        cleaned_data = super(RegistrationForm , self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+        
+        if password != confirm_password:
+            raise forms.ValidationError(
+                'Password does not match!'
+            )
+            
+            
+    
+    
     def __init__(self,*args,**kwargs):
         super(RegistrationForm,self).__init__(*args,**kwargs)
         self.fields['first_name'].widget.attrs['placeholder'] = 'Enter FirstName'
@@ -27,3 +42,4 @@ class RegistrationForm(forms.ModelForm):
         for field in self.fields:
             self.fields[field].widget.attrs['class'] = 'form-control'
         
+       
