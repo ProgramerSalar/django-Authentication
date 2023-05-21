56b27a1d6d4aef255bcc3238776a45f2b2bbe1f0
ProgramerSalar <manishkumar60708090@gmail.com>
2023-05-21 12:59:49 +0530

    Registeration and login Activation using the Gmail

--- Authentication/settings.py
+++ Authentication/settings.py
@@ -127,3 +127,11 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# SMTP configuration 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587 
+EMAIL_HOST_USER = 'lightweightprograme@gmail.com'
+EMAIL_HOST_PASSWORD = 'qingciaxhmqxirtt'
+EMAIL_USE_TLS = True 
--- accounts/views.py
+++ accounts/views.py
@@ -6,8 +6,19 @@ from django.contrib import messages , auth
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_decode , urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
+from django.views.decorators.csrf import csrf_protect
+from django.views.decorators.csrf import requires_csrf_token
 
 
+
+
+@requires_csrf_token
 def register(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
@@ -18,9 +29,27 @@ def register(request):
             email = form.cleaned_data['email']
             password = form.cleaned_data['password']
             username = email.split('@')[0]
-            user = Account.objects.create_user(first_name = first_name , last_name = last_name , email = email , username = username , password = password)
-            user.phone_number = phone_number 
+            user = Account.objects.create_user(first_name=first_name ,
+                                               last_name=last_name , email=email ,
+                                               username=username , password=password)
+            user.phone_number=phone_number 
             user.save()
+            
+            
+            # user activation
+            current_site = get_current_site(request)
+            mail_subject = 'Please activate your account'
+            message = render_to_string('accounts/account_verification_email.html',{
+                'user':user,
+                'domain':current_site,
+                'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+                'token':default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
+            messages.success(request, 'Register successful')
+            return redirect('register')
             
     else:
         form=RegistrationForm()
@@ -30,6 +59,8 @@ def register(request):
         'form':form,
     }
     return render(request , 'accounts/register.html',context)
+
+
 
 
 def login(request):
@@ -46,11 +77,6 @@ def login(request):
             return redirect('login')
     return render(request , 'accounts/login.html')
 
-
-
-
-
-
 @login_required(login_url = 'login')
 def logout(request):
     auth.logout(request)
@@ -58,3 +84,21 @@ def logout(request):
     return redirect('login')
 
 
+
+def activate(request, uidb64 , token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = Account._default_manager.get(pk=uid)
+
+    except(TypeError,ValueError,OverflowError,Account.DoesNotExist):
+        user = None 
+
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request , 'Configuration your account is activated')
+        return redirect('login')
+
+    else:
+        messages.error(request, 'Invalid activation link')
+        return redirect('register')
